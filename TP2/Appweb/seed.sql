99d68ad4f5ae7cf4b88abb031e2568ba142b8f92
aamine33 <abatman754@gmail.com>
2025-01-13 16:49:01 +0100

    *

--- TP2/Appweb/seed.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE IF NOT EXISTS docker_database;
-
-USE docker_database;
-
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL
-);
---
+++ TP2/Apweb/seed.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
+);
+
+INSERT INTO users (username, email) VALUES ('admin', 'admin@example.com');
